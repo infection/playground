548eaefb0a8aed1b743408af6729dfecaa4c3710
borNfreee <b0rn@list.ru>
2019-03-02 23:56:04 +0300

    First controller

---
+++ app/src/Controller/PlaygroundController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PlaygroundController
+{
+    /**
+     * @Route(name="playground_index", path="/hello")
+     */
+    public function hello(): Response
+    {
+        return new Response('Hello!');
+    }
+}
