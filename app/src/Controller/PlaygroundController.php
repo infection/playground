b6a2d2a1c373c7ea8e2e2c074c0eb8326b5abe55
borNfreee <b0rn@list.ru>
2019-03-03 03:05:34 +0300

    Add WebProfiles / twig components

--- app/src/Controller/PlaygroundController.php
+++ app/src/Controller/PlaygroundController.php
@@ -4,16 +4,19 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class PlaygroundController
+class PlaygroundController extends AbstractController
 {
     /**
      * @Route(name="playground_index", path="/")
      */
     public function hello(): Response
     {
-        return new Response('Hello - <> from volume!');
+        return $this->render('playground/hello.html.twig', [
+            'name' => 'Infection',
+        ]);
     }
 }
