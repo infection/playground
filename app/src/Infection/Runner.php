028ae9e1985037d05715524aea7f0da04bc346f1
Maks Rafalko <maks.rafalko@gmail.com>
2026-02-15 11:34:31 +0100

    Do not show mutations in the output (#12)

--- app/src/Infection/Runner.php
+++ app/src/Infection/Runner.php
@@ -84,7 +84,16 @@ class Runner
         }
 
         try {
-            $process = new Process(['php', '--define', 'memory_limit=100M', '../infection.phar', '--log-verbosity=all', '--ansi', '--no-progress'], $rootDir);
+            $process = new Process([
+                'php',
+                '--define',
+                'memory_limit=100M',
+                '../infection.phar',
+                '--log-verbosity=all',
+                '--ansi',
+                '--no-progress',
+                '--show-mutations=0'
+            ], $rootDir);
             $process->setTimeout(self::PROCESS_TIMEOUT_SEC);
 
             $process->run();
