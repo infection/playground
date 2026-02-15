adf38539593beca423768f39e748d45b7a62c2d4
Maks Rafalko <maks.rafalko@gmail.com>
2026-02-15 12:07:59 +0100

    Add deploy script

---
+++ deploy/prod.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$(dirname "$0")"
+
+docker compose -f docker-compose.prod.yml build --no-cache --force-rm
+
+"$SCRIPT_DIR/push_images.sh"
+
+rm -rf docker.sock
+ssh -nNT -L ./docker.sock:/var/run/docker.sock $(cat "$SCRIPT_DIR/../secrets/ssh_user")@$(cat "$SCRIPT_DIR/../secrets/ssh_host") & echo $! > /tmp/tunnel.pid
+
+echo "Waiting for SSH tunnel..."
+while [ ! -S ./docker.sock ]; do sleep 0.5; done
+echo "SSH tunnel established."
+
+export DOCKER_HOST=unix://$(pwd)/docker.sock
+export DOCKER_REGISTRY=maksrafalko
+export APP_SECRET=$(cat "$SCRIPT_DIR/../secrets/app_secret")
+export DATABASE_NAME=$(cat "$SCRIPT_DIR/../secrets/mysql_db_name")
+export DATABASE_USER=$(cat "$SCRIPT_DIR/../secrets/mysql_db_user")
+
+docker stack deploy -c docker-stack.yml infection --with-registry-auth
+
